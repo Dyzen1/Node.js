4bd119227586c100a78f5375ddabb5b14736cda2
Dyzen1 <117169978+Dyzen1@users.noreply.github.com>
2024-11-27 16:29:10 +0200

    Delete task3.js

--- task3.js
+++
@@ -1,9 +0,0 @@
-const values = [0, 7, 0, -3, 12, 0, 4, 0, 0, 8]; //  array of numbers 
-
-let zeroCount = 0; // count variable 
-
-for (let index = 0; index < values.length; index++) {
-    zeroCount += Number(values[index] === 0); // Checking if the number is zero, incrementing count if true
-}
-
-console.log(`Number of zeros in the array: ${zeroCount}`); // Updated message
